9191599b8ee5c809b98a4b5f64f3df1f51f7c6e9
anxdpanic <anxdpanic@users.noreply.github.com>
2020-09-13 12:32:56 -0400

    Initial commit

---
+++ resources/lib/service.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2020 Tubed API (script.module.tubed.api)
+
+    This file is part of script.module.tubed.api
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+    See LICENSES/GPL-2.0-or-later.txt for more information.
+"""
