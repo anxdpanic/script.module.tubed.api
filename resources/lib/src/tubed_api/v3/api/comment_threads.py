b75598553bbbe835c15511772e70d11610bdc084
anxdpanic <anxdpanic@users.noreply.github.com>
2020-10-03 20:00:54 -0400

    allow unauthorized comment/commentThread get requests

--- resources/lib/src/tubed_api/v3/api/comment_threads.py
+++ resources/lib/src/tubed_api/v3/api/comment_threads.py
@@ -14,8 +14,8 @@ from ..query import query
 
 # https://developers.google.com/youtube/v3/docs/commentThreads/list
 @query
-def get(parameters=None):
-    return V3Query('get', 'commentThreads', parameters=parameters)
+def get(parameters=None, unauthorized=False):
+    return V3Query('get', 'commentThreads', parameters=parameters, unauthorized=unauthorized)
 
 
 # https://developers.google.com/youtube/v3/docs/commentThreads/insert
